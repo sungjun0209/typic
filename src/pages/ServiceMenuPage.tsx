4a2f57642c29187b5ca65874b986a736c59044b9
seotjuu <tjrenffl8@naver.com>
2025-06-07 16:04:13 +0900

    [fix] arrow add

--- src/pages/ServiceMenuPage.tsx
+++ src/pages/ServiceMenuPage.tsx
@@ -1,3 +1,4 @@
+import { MdOutlineArrowForwardIos, MdOutlineArrowBackIos } from "react-icons/md";
 import { serviceMenuData } from "@app/constants/serviceMenuData";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -19,19 +20,34 @@ const ServiceMenuPage = () => {
         <div className="w-full h-full">
             {
                 serviceMenuData.map((item, index_st) => (
-                    <div key={index_st} className={`asdsad w-full h-full flex flex-col justify-between ${present === index_st ? 'flex' : "hidden"}`}
+                    <div key={index_st} className={`w-full h-full flex flex-col justify-between ${present === index_st ? 'flex' : "hidden"}`}
                         style={{ backgroundColor: item.bgColor }}
                     >
-                        <div className={`h-2/3 flex items-center justify-center  bg-[${item.bgColor}]`}>
+                        <div className={`h-2/3 flex items-center justify-center bg-[${item.bgColor}] relative`}>
+                            {
+                                index_st !== 0 ?
+                                    <button onClick={() => setPresent(index_st - 1)} className="absolute left-[0px] top-1/2 border-none rounded-[5px] bg-transparent hover:brightness-75 ">
+                                        <MdOutlineArrowBackIos size={50} color="gray" className="cursor-pointer" />
+                                    </button>
+                                    : null
+                            }
                             <img key={index_st} src={item.src} className="w-full" />
+                            {
+                                index_st !== serviceMenuData.length - 1 ?
+                                    <button onClick={() => setPresent(index_st + 1)} className="absolute right-[0px] top-1/2 border-none rounded-[5px] bg-transparent hover:brightness-75">
+                                        <MdOutlineArrowForwardIos size={50} color="gray" className="cursor-pointer" />
+                                    </button>
+                                    : null
+                            }
                         </div>
+
                         <div className="w-full h-1/3 flex flex-col items-center justify-between bg-[#ffffff] rounded-t-[10px] p-[20px] shadow-[10px]">
                             <div className="flex flex-col items-center gap-[10px]">
                                 <h2>{item.title}</h2>
                                 <p className="text-[13px] text-[#666666] whitespace-pre-line">{item.description}</p>
                             </div>
                             <div className="w-full flex flex-col items-center gap-[10px]">
-                                <div className="w-full flex itmes-center justify-center gap-[5px]">
+                                <div className="w-full flex items-center justify-center gap-[5px]">
                                     {
                                         serviceMenuData.map((_, index_nd) => (
                                             <button key={index_nd} id={index_nd.toString()} onClick={handleClick} className={`w-[15px] h-[15px] rounded-[100%] border-none bg-[${present === index_nd ? '#3c388f' : ''}]`}></button>
