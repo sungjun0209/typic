b48076b8fba97ba4b4099a48015d555d39127e3d
sungjun0209 <yoonsungjun29@gmail.com>
2025-06-07 15:21:53 +0900

    profile 안성

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from "react";
+import type { UserInfo } from "../types/user";
+
+const defaultUser: UserInfo = {
+  name: "",
+  age: "",
+  gender: "남성",
+  mbti: "",
+  image: null,
+};
+
+const UserContext = createContext<{
+  user: UserInfo;
+  setUser: React.Dispatch<React.SetStateAction<UserInfo>>;
+} | null>(null);
+
+export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<UserInfo>(defaultUser);
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) throw new Error("UserContext 사용 오류");
+  return context;
+};
